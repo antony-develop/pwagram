f21fd06f6eea33ddb8ee09751c04048101d27bdc
Anton Kulaga <19634898+antony-develop@users.noreply.github.com>
2019-02-07 10:40:32 +1000

    use cache only strategy for static assets

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,19 @@
 const STATIC_CACHE_NAME = 'static_assets_v7';
 const DYNAMIC_CACHE_NAME = 'dynamic_assets_v1';
+const STATIC_ASSETS = [
+    '/',
+    '/index.html',
+    '/offline.html',
+    '/src/js/app.js',
+    '/src/js/feed.js',
+    '/src/js/material.min.js',
+    '/src/css/app.css',
+    '/src/css/feed.css',
+    '/src/images/main-image.jpg',
+    'https://fonts.googleapis.com/css?family=Roboto:400,700',
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+    'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
+];
 
 self.addEventListener('install', (event) => {
     console.log('[Service Worker] Installing Service Worker ...', event);
@@ -8,20 +22,7 @@ self.addEventListener('install', (event) => {
         caches.open(STATIC_CACHE_NAME)
             .then((cache) => {
                console.log('[Service Worker] Precaching app shell');
-               cache.addAll([
-                   '/',
-                   '/index.html',
-                   '/offline.html',
-                   '/src/js/app.js',
-                   '/src/js/feed.js',
-                   '/src/js/material.min.js',
-                   '/src/css/app.css',
-                   '/src/css/feed.css',
-                   '/src/images/main-image.jpg',
-                   'https://fonts.googleapis.com/css?family=Roboto:400,700',
-                   'https://fonts.googleapis.com/icon?family=Material+Icons',
-                   'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
-               ]);
+               cache.addAll(STATIC_ASSETS);
 
                // add polyfills for performance
                cache.addAll([
@@ -50,31 +51,37 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-            .then((response) => {
-                if (response) {
-                    return response;
-                } else {
-                    return fetch(event.request)
-                        .then((res) => {
-                            return caches.open(DYNAMIC_CACHE_NAME)
-                                .then((cache) => {
-                                    cache.put(event.request.url, res.clone());
-                                    return res;
-                                });
-                        })
-                        .catch((error) => {
-                            return caches.open(STATIC_CACHE_NAME)
-                                .then(cache => {
-                                    if (event.request.url.indexOf('/help')) {
-                                        return cache.match('/offline.html');
-                                    }
-                                });
-                        });
-                }
-            })
-    );
+    if (STATIC_ASSETS.includes(event.request.url)) {
+        event.respondWith(
+            caches.match(event.request)
+        );
+    } else {
+        event.respondWith(
+            caches.match(event.request)
+                .then((response) => {
+                    if (response) {
+                        return response;
+                    } else {
+                        return fetch(event.request)
+                            .then((res) => {
+                                return caches.open(DYNAMIC_CACHE_NAME)
+                                    .then((cache) => {
+                                        cache.put(event.request.url, res.clone());
+                                        return res;
+                                    });
+                            })
+                            .catch((error) => {
+                                return caches.open(STATIC_CACHE_NAME)
+                                    .then(cache => {
+                                        if (event.request.url.indexOf('/help')) {
+                                            return cache.match('/offline.html');
+                                        }
+                                    });
+                            });
+                    }
+                })
+        );
+    }
 });
 
 // Cache-only strategy
