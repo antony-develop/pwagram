78c276ebb9c3286c0ccdbbc0cbd072524082e308
Anton Kulaga <19634898+antony-develop@users.noreply.github.com>
2019-03-16 20:59:45 +1000

    write firebase endpoint for sending posts to

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,31 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const cors = require('cors')({
+    origin: true
+});
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.storePostData = functions.https.onRequest((request, response) => {
+    cors((request, response) => {
+        const post = request.body;
+        admin.database('posts').push({
+            id: post.id,
+            title: post.title,
+            location: post.location,
+            image:  post.image
+        })
+            .then(() => {
+                response.status(201).json({
+                    message: 'Data have been added',
+                    id: post.id
+                });
+            })
+            .catch(error => {
+                response.status(500).json({
+                    error: error
+                });
+            })
+    });
+});
